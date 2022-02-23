20191661ef48627a6f16b5908d0baa2b03a70c5e
saretotracy <tracysareto@gmail.com>
2022-02-23 16:38:37 +0300

    create a get_movies() function that takes in a movie category as an argument.

--- app/request.py
+++ app/request.py
@@ -10,3 +10,24 @@ api_key = app.config['MOVIE_API_KEY']
 
 # Getting the movie base url
 base_url = app.config["MOVIE_API_BASE_URL"]
+
+#create a get_movies() function that takes in a movie category as an argument.
+def get_movies(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+
+
+    return movie_results
+
