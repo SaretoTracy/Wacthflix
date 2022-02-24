7309fe266b2b7589b90207af333bd5ecbddc1428
saretotracy <tracysareto@gmail.com>
2022-02-24 13:35:08 +0300

    create a get_movie_details URL by formatting the base URL with the idand API key.

--- app/request.py
+++ app/request.py
@@ -12,24 +12,25 @@ api_key = app.config['MOVIE_API_KEY']
 base_url = app.config['MOVIE_API_BASE_URL']
 
 #create a get_movies() function that takes in a movie category as an argument.
-def get_movies(category):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_movies_url = base_url.format(category,api_key)
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
 
-    with urllib.request.urlopen(get_movies_url) as url:
-        get_movies_data = url.read()
-        get_movies_response = json.loads(get_movies_data)
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
 
-        movie_results = None
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
 
-        if get_movies_response['results']:
-            movie_results_list = get_movies_response['results']
-            movie_results = process_results(movie_results_list)
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
 
-
-    return movie_results
+    return movie_object
 
 #create a function process_results() that takes in a list of dictionaries.
 
