9afcafeea562928d6a34857bffb8bd43a0ed9feb
saretotracy <tracysareto@gmail.com>
2022-02-24 16:13:29 +0300

    get_movie_details URL by formatting the base URL with the idand API key.

--- app/request.py
+++ app/request.py
@@ -12,6 +12,27 @@ api_key = app.config['MOVIE_API_KEY']
 base_url = app.config['MOVIE_API_BASE_URL']
 
 #create a get_movies() function that takes in a movie category as an argument.
+
+
+def get_movies(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_movies_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+
+
+    return movie_results
+
 def get_movie(id):
     get_movie_details_url = base_url.format(id,api_key)
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
 
 @app.route('/')
 def index():
@@ -17,15 +17,16 @@ def index():
     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
     
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int_id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    movie_id=1234 
+    movie = get_movie(id)
+    title = f'{movie.title}'
 
-    return render_template('movie.html',id = movie_id)
+    return render_template('movie.html',title = title,movie = movie)
 
 
 
