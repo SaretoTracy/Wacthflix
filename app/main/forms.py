c06a4d6684f1e807ea0ab7bf8d28c6f7b7323fa0
saretotracy <tracysareto@gmail.com>
2022-03-02 23:20:07 +0300

    fix review bug

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import InputRequired
+from wtforms import StringField, TextAreaField,SubmitField
+from wtforms.validators import InputRequired 
 
 class ReviewForm(FlaskForm):
-
     title = StringField('Review title',validators=[InputRequired()])
-    review = TextAreaField('Movie review', validators=[InputRequired()])
+    review = TextAreaField('Movie review',validators=[InputRequired()])
     submit = SubmitField('Submit')
